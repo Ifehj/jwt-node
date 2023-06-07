e9bf1395a00c63d4916023233388450660f7d740
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-06-07 15:05:46 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const dashboardRoute = require('./routers/dashboardRouter')
+const loginRoute = require('./routers/authRoute')
 
-app.use("/dashboardRoute", dashboardRoute)
+app.use(express.json());
+app.use("/v1", dashboardRoute)
+app.use("/v2", loginRoute)
 
 let PORT = 3000
 
